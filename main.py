21c98d21d5871ad6c487d27bb6df03cef933aa01
Andrea <andrea.piattino@phase.eu>
2023-12-25 12:13:52 +0100

    First commit.

---
+++ main.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+# Acquisisci l'audio dal microfono
+with sr.Microphone() as source:
+    print("Parla qualcosa...")
+    try:
+        audio_data = recognizer.listen(source, timeout=3)
+    except sr.WaitTimeoutError:
+        print("Nessun audio rilevato entro il timeout.")
+
+# Usa il riconoscitore per ottenere il testo
+try:
+    text = recognizer.recognize_google(audio_data, language="it-IT")
+    print("Testo riconosciuto:", text)
+except sr.UnknownValueError:
+    print("Il riconoscitore non ha compreso l'audio.")
+except sr.RequestError as e:
+    print(f"Errore nella richiesta al servizio di riconoscimento vocale; {e}")
